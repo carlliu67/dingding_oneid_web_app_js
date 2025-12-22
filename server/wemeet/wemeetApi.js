cb58bc32331a3e22b90f99162228e4adbf9fe093
liuqi <liuqi92@foxmail.com>
2025-12-22 16:46:28 +0800

    chore: 更新版本号至1.0.4
    更新package.json、package-lock.json和wemeetApi.js中的版本号，保持版本一致

--- server/wemeet/wemeetApi.js
+++ server/wemeet/wemeetApi.js
@@ -6,7 +6,7 @@ import serverConfig from '../config/server_config.js';
 import { isLogin, getUserid } from '../dingtalkapi/dingtalkAuth.js';
 
 const USER_INFO_KEY = 'user_info';
-const WEMEET_VERSION = 'wemeet-dingtalk-js/v1.0.3'
+const WEMEET_VERSION = 'wemeet-dingtalk-js/v1.0.4'
 
 /**
  * 生成签名函数
